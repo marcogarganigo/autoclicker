fbf77f597ae30e1ac19e49a7f51407a77fcfdaa8
Marco Garganigo <marco.garganigo@swissbix.ch>
2022-12-19 17:00:49 +0100

    update

--- autoClicker.py
+++ autoClicker.py
@@ -1,1 +1,32 @@
+import pyautogui
+import keyboard
+import appJar
 
+# keyboard does not work
+
+
+def buttonPress(btn):
+    if btn == 'Start(s)' or keyboard.read_key('s') == 's':
+        amount = app.getEntry('amount')
+        btn = app.getRadioButton('click')
+        if (btn == 'right button'):
+            btn = 'right'
+        else:
+            btn = 'left'
+        for _ in range(int(amount)):
+            pyautogui.click(button=btn)
+
+
+app = appJar.gui('autoClicker')
+app.setSize(300, 200)
+app.setSticky('news')
+app.addLabel('title', 'Auto Clicker')
+app.addEntry('amount')
+app.addButton('Start(s)', buttonPress, row=3)
+app.setSticky('e')
+app.radioButton('click', 'right button', row=2)
+app.setSticky('w')
+app.radioButton('click', 'left button', row=2)
+
+
+app.go()
